41e5e199291321ef9d68e7fd814415b17c460c3f
Diego Balmaceda <designxaes@gmail.com>
2020-01-05 13:11:20 -0600

    Agregado: Metodo para extraer, agregar y borrar existencias. Agregado: Unit Tests para metodos anteriormente mencionados.

--- Domain/Inventory/Existencia.cs
+++ Domain/Inventory/Existencia.cs
@@ -9,11 +9,13 @@ namespace Domain.Inventory
 {
     public class Existencia
     {
-        
+
         public int Existencia_ID, FK_Repuesto_ID, Unidades;
         public string FK_Localizacion_ID;
-        
-        public Existencia() {}
+
+        public Existencia()
+        {
+        }
 
         private Existencia(int existenciaId, int fkRepuestoId, int unidades, string localizacionId)
         {
@@ -25,28 +27,75 @@ namespace Domain.Inventory
 
         public static Existencia AgregarExistencia(int fkRepuestoId, int unidades, string fkLocalizacionId)
         {
-            
+
             // Validar si algun parametro esta fuera del minimo aceptable.
 
             if (unidades <= 0)
-                throw new ArgumentOutOfRangeException("", "Las Unidades deberian ser mayores a 0. " + 
+                throw new ArgumentOutOfRangeException("", "Las Unidades deberian ser mayores a 0. " +
                     "El Costo Unitario debe ser mayor o igual que 0.");
-            
+
             try
             {
-                const string sqlString = 
+                const string sqlString =
                     "Insert Into Existencia (FK_Repuesto_ID, Unidades, FK_Localizacion_ID)" +
                     "Values (@fkRepuestoId, @unidades, @fkLocalizacionId); Select Cast(SCOPE_IDENTITY() as int)";
 
                 var id = DbCliente.GetConexion()
-                    .Execute(sqlString, new {fkRepuestoId, unidades, fkLocalizacionId });
-                
+                    .Execute(sqlString, new {fkRepuestoId, unidades, fkLocalizacionId});
+
                 return new Existencia(id, fkRepuestoId, unidades, fkLocalizacionId);
             }
             catch (SqlException ex)
             {
                 throw SqlExceptionMapper.Map(ex);
             }
+
+        }
+
+        public Existencia ExtraerUnidades(int cantidad)
+        {
+            
+            if(cantidad > this.Unidades)
+                throw new InvalidOperationException("No se pueden extraer mas unidades que las existentes.");
+
+            if (cantidad == this.Unidades)
+            {
+                this.Unidades = 0;
+                this.Eliminar();
+                return this;
+            }
+            
+            var nuevaCantidad = this.Unidades - cantidad;
+            const string sqlString = "Update Existencia Set Unidades = @nuevaCantidad Where Existencia_ID = @Existencia_ID;";
+            DbCliente.GetConexion().Execute(sqlString, new { nuevaCantidad, this.Existencia_ID });
+            this.Unidades = nuevaCantidad;
+            return this;
+            
+        }
+        
+        public Existencia AgregarUnidades(int cantidad)
+        {
+            
+            // Checkear si cantidad proporcionada es mayor a 0.
+            
+            if(cantidad < 1)
+                throw new ArgumentOutOfRangeException(nameof(cantidad), "Cantidad debe ser mayor a 0. Para extraer, use el metodo Extraer Unidades");
+            
+            var nuevaCantidad = cantidad + this.Unidades;
+            const string sqlString = "Update Existencia Set Unidades = @nuevaCantidad Where Existencia_ID = @Existencia_ID;";
+            DbCliente.GetConexion().Execute(sqlString, new { nuevaCantidad, this.Existencia_ID });
+            this.Unidades = nuevaCantidad;
+            return this;
+            
+        }
+
+        private void Eliminar()
+        {
+            if(this.Unidades != 0)
+                throw new InvalidOperationException("No se puede eliminar una existencia que tiene todavia unidades presentes.");
+            
+            const string sqlString = "Delete From Existencia Where Existencia_ID = @Existencia_ID";
+            DbCliente.GetConexion().Execute(sqlString, new { this.Existencia_ID });
         }
 
         public static List<Existencia> GetExistencias()
@@ -59,6 +108,12 @@ namespace Domain.Inventory
         {
             const string sqlString = "Select * From Existencia Where Existencia_ID = @existenciaId";
             return DbCliente.GetConexion().QueryFirstOrDefault<Existencia>(sqlString, new { existenciaId });
+        }
+
+        public static List<Existencia> FindExistenciasByRepuesto(int repuestoId)
+        {
+            const string sqlString = "Select * From Existencia Where FK_Repuesto_ID = @FK_Repuesto_ID";
+            return DbCliente.GetConexion().Query<Existencia>(sqlString, new { repuestoId }).ToList();
         }
 
         public override string ToString()
--- DomainTesting/StockTest.cs
+++ DomainTesting/StockTest.cs
@@ -52,5 +52,73 @@ namespace DomainTesting
                 Existencia.AgregarExistencia(1, 0, Localizacion.GetLocalizaciones()[0].Codigo);
             }, "[ERROR]: Una excepcion ArgumentOutOfRangeException deberia ser lanzada en creacion de Existencia cuando las unidades son 0 o negativas.");
         }
+
+        [Test]
+        public void AgregarExistencias()
+        {
+            
+            this.PopularExistencias();
+            var existencia = Existencia.FindExistencia(1);
+            
+            Assert.Multiple(() =>
+            {
+                
+                existencia.AgregarUnidades(5);
+                
+                // Checkear valor en Memoria.
+                
+                Assert.AreEqual(15, existencia.Unidades);
+                
+                // Checkear valor en la Base de Datos.
+
+                existencia = Existencia.FindExistencia(1);
+                Assert.AreEqual(15, existencia.Unidades);
+                
+                // Checkear lanzamiento de Exception.
+
+                Assert.Throws<ArgumentOutOfRangeException>(() => existencia.AgregarUnidades(-1));
+
+            });
+            
+        }
+
+        [Test]
+        public void ExtraerExistencias()
+        {
+            
+            this.PopularExistencias();
+            var existencia = Existencia.FindExistencia(1);
+            
+            Assert.Multiple(() =>
+            {
+                
+                existencia.ExtraerUnidades(5);
+                
+                // Checkear valor en Memoria.
+                
+                Assert.AreEqual(5, existencia.Unidades);
+                
+                // Checkear valor en Base de Datos.
+                
+                existencia = Existencia.FindExistencia(1);
+                Assert.AreEqual(5, existencia.Unidades);
+                
+                // Checkear lanzamiento de Exception.
+
+                Assert.Throws<InvalidOperationException>(() => existencia.ExtraerUnidades(10));
+                
+                // Extrayendo todas las Unidades.
+
+                existencia.ExtraerUnidades(5);
+                Assert.AreEqual(0, existencia.Unidades);
+                
+                // La entidad en la Base de Datos deberia ser borrada.
+
+                existencia = Existencia.FindExistencia(1);
+                Assert.IsNull(existencia);
+
+            });
+            
+        }
     }
 }
